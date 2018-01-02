3e64f7e39d1ab99e7d1ec1c190860ea726d5fc42
GrIvA <griva99@gmail.com>
2018-01-02 17:43:46 +0200

    Update gList.js
    Refactor directive.

--- gList.js
+++ gList.js
@@ -1,69 +1,66 @@
-(function () {
-    angular.module("gTools")
-    .directive("gList", [ '$compile', function ($compile) {
-        return {
-            "scope": {
-                "items": "<",
-                "cb" : "&callback",
-                "tag": "childtag"
-            },
-            restrict: 'EA',
-            transclude: true,
-            controller: function($scope) {
-                /*
-                this.$doCheck = function() {
-                    console.log($scope);
-                    console.log('doCheck: time mutated: ' + new Date().getTime());
-                };
-                this.$postLink = function() {
-                    console.log('postLink: time mutated: ' + new Date().getTime());
-                    console.log($scope);
-                };
-                */
-            },
-            compile: function(elem, attrs) {
-                function render(scope, elem, transclude) {
-                    //reset all elements
-                    elem.html('');
-                    while (scope.$$childTail) scope.$$childTail.$destroy();
+(function (angular, module) {
+    module.directive('grList', ['$compile', function($compile) {
+        function controller($scope, $element) {
+            $scope.tag = $scope.grListTag || $element[0].nodeName;
+            $scope.$on('grListClick', function(e, item) {
+                $scope.grListCb && $scope.grListCb(item);
+            });
+        }
 
-                    angular.element.each(scope.items, function(key, item) {
-                        var
-                            childScope = scope.$new(),
-                            tag = scope.tag || 'li',
-                            childElem = angular.element('<' + tag + ' />');
+        function compile(elem, attrs) {
+            function render(scope, elem, transclude) {
+                //reset all elements
+                elem.html('');
+                while (scope.$$childTail) scope.$$childTail.$destroy();
 
-                        childScope.item = item;
-                        childScope.clickDispatcher = function() {
-                            childScope.$parent.cb(childScope);
-                        };
+                scope.grListItems && angular.forEach(scope.grListItems, function(item, key) {
+                    var
+                        childScope = scope.$new(),
+                        childElem = angular.element('<' + scope.tag + ' />');
 
-                        childElem.bind('click', childScope.clickDispatcher);
+                    childScope.item = item;
 
-                        //do the transclusion
-                        transclude(childScope, function(clone, innerScope) {
-                            //clone is a copy of the transcluded DOM element content
-                            childElem.append($compile(clone)(innerScope));
-                        });
+                    childElem.bind('click', function(e) {
+                        childScope.$emit('grListClick', childScope.item);
+                    });
 
-                        elem.append(childElem);
+                    //do the transclusion
+                    transclude(childScope, function(clone, innerScope) {
+                        //clone is a copy of the transcluded DOM element content
+                        childElem.append($compile(clone)(innerScope));
+                    });
+
+                    elem.append(childElem);
+                });
+            }
+
+            return {
+                pre: function(scope, elem, attrs, ctrl, transclude) {
+                    render(scope, elem, transclude);
+                    $compile(elem.contents())(scope);
+                },
+                post: function(scope, elem, attrs, ctrl, transclude) {
+                    // So, our list can rerender automatically,
+                    // but all items must be in "items" array
+                    scope.$watch('grListItems.length', function(newValue, oldValue, scope) {
+                        if (!angular.equals(oldValue, newValue)) {
+                            render(scope, elem, transclude);
+                        }
                     });
                 }
+            };
+        }
 
-                return {
-                    pre: function(scope, elem, attrs, ctrl, transclude) {
-                        render(scope, elem, transclude);
-                        $compile(elem.contents())(scope);
-                    },
-                    post: function(scope, elem, attrs, ctrl, transclude) {
-                        scope.$watch('items', function(newValue, oldValue, scope) {
-                            if (!angular.equals(oldValue, newValue)) {
-                                render(scope, elem, transclude);
-                            }
-                        });
-                    }
-                };
+        return {
+            "scope": {
+                "grListItems": "<",
+                "grListTag": "@",
+                "grListCb": "<"
             },
+            restrict: 'EA',
+            controller: controller,
+            compile: compile,
+            transclude: true,
         };
     }]);
-})();
+ })(angular, app);
